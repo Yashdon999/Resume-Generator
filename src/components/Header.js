edf20745c5911fbe25b4b1e35fce94c7f1c45559
Khizarshah01 <khizar.shah010@gmail.com>
2023-02-23 09:35:15 +0530

    Change in header or footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,48 @@
-import React, { useState } from 'react';
-import './Css/header.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import styled from 'styled-components';
 
-export default function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
-  
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-  
-  const navLinks = [
-    { text: 'Home', url: '#' },
-    { text: 'About', url: '#' },
-    { text: 'Contact', url: '#' },
-  ];
-
+function Header() {
   return (
-    <header className="header">
-      <div className="header-logo">My Website</div>
-      <nav className={`header-nav ${menuOpen ? 'open' : ''}`}>
-        <button className="header-nav-toggle" onClick={toggleMenu}>
-          <span></span>
-          <span></span>
-          <span></span>
-        </button>
-        <ul className="header-nav-list">
-          {navLinks.map((link, index) => (
-            <li key={index} className="header-nav-item">
-              <a className="header-nav-link" href={link.url}>{link.text}</a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <StyledNavbar bg="light" expand="lg">
+      <Container fluid>
+        <StyledNavbar.Brand href="#">Resume Generator</StyledNavbar.Brand>
+        <StyledNavbar.Toggle aria-controls="navbarScroll" />
+        <StyledNavbar.Collapse id="navbarScroll">
+          <NavLinks navbarScroll>
+            <NavLink href="#home">Home</NavLink>
+            <NavLink href="#about">About</NavLink>
+            <NavLink href="#contact">Contact</NavLink>
+          </NavLinks>
+        </StyledNavbar.Collapse>
+      </Container>
+    </StyledNavbar>
   );
 }
+
+const StyledNavbar = styled(Navbar)`
+  padding: 1rem 2rem;
+  /* box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px; */
+  /* box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset; */
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+
+`;
+
+const NavLinks = styled(Nav)`
+  max-height: 100px;
+  overflow-y: auto;
+  @media (max-width: 768px) {
+    max-height: none;
+  }
+`;
+
+const NavLink = styled(Nav.Link)`
+  color: #333;
+  font-weight: bold;
+  &:hover {
+    color: #555;
+  }
+`;
+
+export default Header;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,26 +1,58 @@
 import React from "react";
-import "./Css/main.css";
 import styled from "styled-components";
-// import backgroundImage from "./images/em1.jpg";
 
 const Main = () => {
-  // //   const backgroundImage = 'https://res.cloudinary.com/people-matters/image/upload/q_auto,f_auto/v1625166167/1625166166.jpg';
   return (
     <Mains>
-      <div className="main">
-      <h1>Main Section</h1>
-    </div>
+      <Heading>ONLINE RESUME BUILDER</Heading>
+      <MainSection className="main">
+        <Button>Create My Resume</Button>
+      </MainSection>
     </Mains>
   );
 };
+
 export default Main;
 
 const Mains = styled.div`
-  // background-image: url('https://res.cloudinary.com/people-matters/image/upload/q_auto,f_auto/v1625166167/1625166166.jpg'); 
-  // margin-top: 0;
-  // width: 800px;
-  // height: 768px;
-  // background-repeat: no-repeat ;
+  min-height: calc(100vh - 100px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 
+const Button = styled.button`
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: #fbb600;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-top: 30px;
 
+  @media (max-width: 768px) {
+    padding: 8px 16px;
+    font-size: 14px;
+    margin-top: 20px;
+    position: absolute;
+    bottom: 30px;
+  }
+`;
+
+const MainSection = styled.div`
+  width: 80%;
+  max-width: 1200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  height: 100%;
+`;
+
+const Heading = styled.h1`
+  font-size: 40px;
+  margin-bottom: 20px;
 `;
