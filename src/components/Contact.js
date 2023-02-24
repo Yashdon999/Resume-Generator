903823e26b52c0826dd3f4335404a0474b30ca58
Shashwat Agrawal <shashwatagrawal473@gmail.com>
2023-02-24 23:05:55 +0530

    refactored contact & footer

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,49 +1,65 @@
 import React from 'react';
-import { Container, Form, Button } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import styled from 'styled-components';
 
 const StyledForm = styled(Form)`
-  background-color: #fff;
-  padding: 20px;
+  background-color: #f9f9f9;
   border-radius: 10px;
-  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.2);
+  box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.2);
+  height: 90vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  padding: 0 2rem;
+
 `;
 
 const StyledButton = styled(Button)`
-  background-color: #007bff;
-  border-color: #007bff;
+  background-color: #1abc9c;
+  border-color: #1abc9c;
+  font-weight: 600;
+  letter-spacing: 1px;
   margin-top: 1rem;
   &:hover {
-    background-color: #0069d9;
-    border-color: #0062cc;
+    background-color: #16a085;
+    border-color: #16a085;
   }
 `;
 
-function Contact() {
+const Contact = () => {
   return (
-    <Container className="my-5 h-100" >
-      <StyledForm>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter your name" />
-        </Form.Group>
-
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
-
-        <Form.Group controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Message</Form.Label>
-          <Form.Control as="textarea" rows={3} placeholder="Enter your message" />
-        </Form.Group>
-
-        <StyledButton type="submit">
-          Submit
-        </StyledButton>
-      </StyledForm>
-    </Container>
+    <section id="contact" className="py-5">
+      <Container>
+        <Row>
+          <Col md={{ span: 8, offset: 2 }}>
+            <StyledForm>
+              <h2 className="text-center mt-4 fs-2 fw-bold">Get in Touch</h2>
+              <Form.Group controlId="formBasicName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter your name" />
+              </Form.Group>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+              <Form.Group controlId="formBasicSubject">
+                <Form.Label>Subject</Form.Label>
+                <Form.Control type="text" placeholder="Enter subject" />
+              </Form.Group>
+              <Form.Group controlId="formBasicMessage">
+                <Form.Label>Message</Form.Label>
+                <Form.Control as="textarea" rows={4} placeholder="Enter your message" />
+              </Form.Group>
+              <StyledButton type="submit" block>
+                Send Message
+              </StyledButton>
+            </StyledForm>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   );
-}
+};
 
 export default Contact;
+
