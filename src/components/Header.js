eb858be809994e1e5e1c0255ee8f086121c4b5e4
Khizarshah01 <khizar.shah010@gmail.com>
2023-02-24 21:04:14 +0530

    add some css in main.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import styled from 'styled-components';
 import { NavLink, Link } from 'react-router-dom';
@@ -24,11 +23,17 @@ function Header() {
 
 const StyledNavbar = styled(Navbar)`
   padding: 1rem 2rem;
-  /* box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1); */
   box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
-  /* box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px; */
   background-color: #fff; 
   border-bottom: 1px solid #e4e4e4;
+
+  @media screen and (max-width: 991.98px) {
+    padding: 1rem;
+  }
+
+  @media screen and (max-width: 767.98px) {
+    padding: 0.5rem;
+  }
 `;
 
 const NavLinks = styled.nav`
@@ -49,36 +54,10 @@ const StyledNavLink = styled(NavLink)`
     text-decoration: none;
     border-bottom: 2px solid #007bff;
   }
+
+  @media screen and (max-width: 767.98px) {
+    margin: 0.5rem;
+  }
 `;
 
 export default Header;
-
-
-// const StyledNavbar = styled(Navbar)`
-//   padding: 1rem 2rem;
-//   box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
-//   background-color: #fff;
-//   border-bottom: 1px solid #e4e4e4;
-// `;
-
-// const NavLinks = styled(Nav)`
-//   display: flex;
-//   align-items: center;
-// `;
-
-// const NavLink = styled(Nav.Link)`
-//   color: #333;
-//   font-weight: 500;
-//   margin: 0 1rem;
-//   &:hover {
-//     color: #555;
-//     text-decoration: none;
-//   }
-//   &.active {
-//     color: #007bff;
-//     text-decoration: none;
-//     border-bottom: 2px solid #007bff;
-//   }
-// `;
-
-// export default Header;
