962a549389492a785673aa7623c748f845bf5ce1
Khizarshah01 <khizar.shah010@gmail.com>
2023-02-26 12:44:10 +0530

    solove issue of navbar and About page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,7 @@ import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import styled from 'styled-components';
 
 const StyledForm = styled(Form)`
-// ye color jyada sahi lag raha badalna mat bhosadiwalo
-  background-color: #a6c5dfe0; 
+  background-color: #264c6be0;
   border-radius: 10px;
   box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.2);
   height: 90vh;
@@ -13,6 +12,7 @@ const StyledForm = styled(Form)`
   justify-content: space-evenly;
   padding: 0 2rem;
   margin-top: 40px;
+
   input[type="text"],
   input[type="email"],
   textarea {
@@ -20,38 +20,49 @@ const StyledForm = styled(Form)`
     border-radius: 5px;
     padding: 0.5rem;
     margin-bottom: 1rem;
-    margin-top: 0.5rem; /* new line */
+    margin-top: 0.5rem;
   }
+
   input[type="text"]:first-of-type {
-    margin-top: 1rem; /* new line */
+    margin-top: 1rem;
+  }
+
+  @media (max-width: 767px) {
+    /* adjust form styling for mobile screens */
+    padding: 0 1rem;
+    height: auto;
+
+    input[type="text"],
+    input[type="email"],
+    textarea {
+      margin-bottom: 0.5rem;
+      margin-top: 0.5rem;
+    }
+
+    input[type="text"]:first-of-type {
+      margin-top: 0.5rem;
+    }
   }
 `;
 
-// const StyledButton = styled(Button)`
-//   background-color: #0077b6; /* new color */
-//   border-color: white; /* new color */
-//   font-weight: 600;
-//   letter-spacing: 1px;
-//   margin: 1rem 0;
-//   &:hover {
-//     background-color: #023e8a; /* new color */
-//     border-color: #023e8a; /* new color */
-//   }
-// `;
-
 const StyledButton = styled(Button)`
-  background-color: #2f63c3; /* new color */
-  /* border-color: white; new color */
+  background-color: #2f63c3;
   box-shadow: 1px 2px 14px 0px black;
   font-weight: 600;
   letter-spacing: 1px;
   margin: 1rem 0;
+
   &:hover {
-    background-color: #5b95ff; /* new color */
-    border-color: #023e8a; /* new color */
+    background-color: #5b95ff;
+    border-color: #023e8a;
+  }
+
+  @media (max-width: 767px) {
+    /* adjust button styling for mobile screens */
+    margin: 1rem auto;
+    width: 100%;
   }
 `;
-
 
 const StyledSection = styled.section`
   background-image: url('https://softlinkoptions.co.ke/wp-content/uploads/2017/09/i-need-a-website-for-my-small-business-where-start1.jpg');
@@ -85,6 +96,8 @@ const Contact = () => {
                 <Form.Control as="textarea" rows={4} placeholder="Enter your message" />
               </Form.Group>
               <StyledButton type="submit" block>
+               
+
                 Send Message
               </StyledButton>
             </StyledForm>
