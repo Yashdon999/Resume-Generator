f5100390bbf5f0284176bf25cf1184fcfe0de3bb
Kalp <kalp2002prajapati@gmail.com>
2023-03-05 15:17:54 +0530

    COntact Page Design done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import styled from "styled-components";
+// import ContactUs from './images/ContactUs.png'
 
 const StyledForm = styled(Form)`
-  background-color: #9fbcbf9e;
-  /* border-radius: 10px; */
-  font-family: Garamond, serif;
+  /* background-color: rgba(0,0,1, 0.2); */
+  background: white;
+  /* box-shadow: 5px 5px 15px #D1D9E6, -5px -5px 15px #ffffff; */
+  /* backdrop-filter: blur(20px); */
+  color: #1A202C;
+  border-radius: 10px;
+  /* font-family: Garamond, serif; */
   box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.2);
-  height: 90vh;
-  width: 90vh;
+  height: 100%;
+  width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
   padding: 0 2rem;
   margin-top: 40px;
-  margin-left: 35px;
+  /* margin-left: 35px; */
   margin-bottom: 35px;
   /* box-shadow: 0px 1px 0px 0px rgba(0,0,0,0.22), 1px 0px 0px 0px rgba(0,0,0,0.22), 1px 2px 0px 0px rgba(0,0,0,0.22), 2px 1px 0px 0px rgba(0,0,0,0.22), 2px 3px 0px 0px rgba(0,0,0,0.22), 3px 2px 0px 0px rgba(0,0,0,0.22), 3px 4px 0px 0px rgba(0,0,0,0.22), 4px 3px 0px 0px rgba(0,0,0,0.22), 4px 5px 0px 0px rgba(0,0,0,0.22), 5px 4px 0px 0px rgba(0,0,0,0.22), 5px 6px 0px 0px rgba(0,0,0,0.22), 6px 5px 0px 0px rgba(0,0,0,0.22), 6px 7px 0px 0px rgba(0,0,0,0.22), 7px 6px 0px 0px rgba(0,0,0,0.22), 7px 8px 0px 0px rgba(0,0,0,0.22), 8px 7px 0px 0px rgba(0,0,0,0.22); */
 
@@ -37,6 +42,8 @@ const StyledForm = styled(Form)`
     height: auto;
     width: 100%;
     margin: 0;
+    margin-top: 40px;
+    margin-bottom: 40px;
 
     input[type="text"],
     input[type="email"],
@@ -72,10 +79,12 @@ const StyledButton = styled(Button)`
 `;
 
 const StyledSection = styled.section`
-  background-image: url("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.meQJ91pe0O1iqNXponnaxAHaEK%26pid%3DApi&f=1&ipt=defe23f251047203dc4a08bfd218744b5f5e92d89fff8e92f18eba786bcbc068&ipo=images");
+  /* background-image: url(); */
+  background:linear-gradient(45deg,#cff2ff,#00dbde);
   background-size: cover;
   background-position: center center;
   min-height: 100vh;
+  height: 100%;
   /* background-color: #00203FFF; */
 `;
 
@@ -83,31 +92,31 @@ const Contact = () => {
   return (
     <StyledSection id="contact">
       <Container>
-        <Row>
-          <Col md={{ span: 8, offset: 2 }}>
+        <Row className="d-flex align-items-center justify-content-center">
+          <Col md={{ span:8 }}>
             <StyledForm>
-              <h2 className="text-center mt-4 fs-2 fw-bold">Get in Touch</h2>
-              <Form.Group controlId="formBasicName">
-                <Form.Label>Name</Form.Label>
+              <h2 className="text-center mt-4 mb-3" style={{fontSize:"40px", fontWeight: "700"}}>Get in Touch</h2>
+              <Form.Group controlId="formBasicName" >
+                <Form.Label >Name</Form.Label>
                 <Form.Control type="text" placeholder="Enter your name" />
               </Form.Group>
               <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
+                <Form.Label >Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
               </Form.Group>
               <Form.Group controlId="formBasicSubject">
-                <Form.Label>Subject</Form.Label>
+                <Form.Label >Subject</Form.Label>
                 <Form.Control type="text" placeholder="Enter subject" />
               </Form.Group>
               <Form.Group controlId="formBasicMessage">
-                <Form.Label>Message</Form.Label>
+                <Form.Label >Message</Form.Label>
                 <Form.Control
                   as="textarea"
                   rows={4}
                   placeholder="Enter your message"
                 />
               </Form.Group>
-              <StyledButton type="submit" block>
+              <StyledButton type="submit" block className="form_button">
                 Send Message
               </StyledButton>
             </StyledForm>
