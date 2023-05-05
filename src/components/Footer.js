52cd0b849ed90bfae5fa26b908a074e74805934f
Mr.Cracker <77793128+MrCracker-OP@users.noreply.github.com>
2023-05-05 20:17:34 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,7 @@ const teamMembers = [
     { name: "Khizar Shah", github: "https://github.com/Khizarshah01" },
     { name: "Shashwat Agrawal", github: "https://github.com/ShashwatAgrawal20" },
     { name: "Yash Vyavahare", github: "https://github.com/Yashdon999" },
+    { name: "Om Ingle", github: "https://github.com/mr-pros" },
 ];
 
 const Footer = () => {
