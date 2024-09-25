d0ad2317969890ac65272e8060bb8c1759788a41
omingle-og <ingleom81@gmail.com>
2024-09-25 15:57:32 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -55,6 +55,11 @@ const About = () => {
         html_url: "https://github.com/yashdon999",
         image: "https://avatars.githubusercontent.com/u/104668751?v=4",
       },
+      {
+        login: "omingle-og",
+        html_url: "https://github.com/omingle-og",
+        image: "https://avatars.githubusercontent.com/u/161492096?v=4",
+      }
     ],
     []
   );
